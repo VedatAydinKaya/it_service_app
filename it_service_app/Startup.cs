635da2729f9bd4ca428d730342fc014f81362710
Vedat Kaya <vedataydinkayaa@gmail.com>
2022-01-25 23:11:29 +0300

    view imports,startup updated and changed

--- it_service_app/Startup.cs
+++ it_service_app/Startup.cs
@@ -12,6 +12,7 @@ namespace it_service_app
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,8 +31,8 @@ namespace it_service_app
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                      name: "default",
+                      pattern: "{controller=Home}/{action=Index}/{id?}");
 
             });
 
