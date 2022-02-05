b3a9ae223acd3fef11528325fb630cdbec6011c4
Vedat Kaya <vedataydinkayaa@gmail.com>
2022-02-05 23:18:45 +0300

    payment6

--- it_service_app/Controllers/PaymentController.cs
+++ it_service_app/Controllers/PaymentController.cs
@@ -1,9 +1,11 @@
-﻿using it_service_app.Models.Payment;
+﻿using it_service_app.Extensions;
+using it_service_app.Models.Payment;
 using it_service_app.Services;
 using it_service_app.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace it_service_app.Controllers
 {
@@ -44,14 +46,30 @@ namespace it_service_app.Controllers
         {
             var paymentModel = new PaymentModel()
             {
-                 Installment= paymentViewModel.Installment,
-                 Address=paymentViewModel.AddressModel,
-                 BasketList=new List<BasketModel>() { paymentViewModel.BasketModel },
-                 CardModel=paymentViewModel.CardModel,
-                 Customer=new CustomerModel(),
-                 Price=1000
-                  
+                Installment = paymentViewModel.Installment,
+                Address = paymentViewModel.AddressModel,
+                BasketList = new List<BasketModel>(),
+                CardModel = paymentViewModel.CardModel,
+                Customer = new CustomerModel(),
+                Price = 1000,
+                UserId = HttpContext.GetUserId(),
+                Ip=Request.HttpContext.Connection.RemoteIpAddress?.ToString(),                  
             };
+
+            var installmentInfo = _paymentService.CheckInstallments(paymentModel.CardModel.CardNumber.Substring(0, 6), paymentModel.Price);
+
+            var installmentNumber = installmentInfo.InstallmentPrices.FirstOrDefault(x => x.InstallmentNumber == paymentViewModel.Installment);
+
+            if (installmentNumber!=null)
+            {
+                paymentModel.PaidPrice = decimal.Parse(installmentNumber.TotalPrice);
+            }
+            else
+            {
+                paymentModel.PaidPrice = decimal.Parse(installmentInfo.InstallmentPrices[0].TotalPrice);
+            }
+
+
             var result = _paymentService.Pay(paymentModel);     
             return View();
         }
--- it_service_app/Services/IyzicoPaymentService.cs
+++ it_service_app/Services/IyzicoPaymentService.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
+using it_service_app.Models.Identity;
 using it_service_app.Models.Payment;
 using Iyzipay.Model;
 using Iyzipay.Request;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using MUsefulMethods;
 using System;
@@ -16,10 +18,12 @@ namespace it_service_app.Services
         private readonly IyzicoPaymentOptions _options;
         private readonly IConfiguration _configuration;
         private readonly IMapper _mapper;
-        public IyzicoPaymentService(IConfiguration configuration,IMapper mapper)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public IyzicoPaymentService(IConfiguration configuration, IMapper mapper, UserManager<ApplicationUser> userManager)
         {
             _configuration = configuration;
             _mapper = mapper;
+            _userManager = userManager;
             var section = _configuration.GetSection(IyzicoPaymentOptions.Key);  // Gets a configuration sub-section with the specified key
 
             _options = new IyzicoPaymentOptions()
@@ -27,15 +31,51 @@ namespace it_service_app.Services
                 ApiKey = section["ApiKey"],
                 SecretKey = section["SecretKey"],
                 BaseUrl = section["BaseUrl"],
-                ThreedsCallbackUrl =section["ThreedsCallbackUrl"]
+                ThreedsCallbackUrl = section["ThreedsCallbackUrl"]
 
-            };  
+            };
         }
-        private string GenerateConverstaionId() 
+        private string GenerateConverstaionId()
         {
             return StringHelpers.GenerateUniqueCode();
         }
 
+        private CreatePaymentRequest InitialPaymentRequest(PaymentModel paymentModel)
+        {
+            var paymentRequest = new CreatePaymentRequest();
+
+            paymentRequest.Installment = paymentModel.Installment;
+            paymentRequest.Locale = Locale.TR.ToString();
+            paymentRequest.ConversationId = GenerateConverstaionId();
+            paymentRequest.Price = paymentModel.Price.ToString();
+            paymentRequest.PaidPrice = paymentModel.PaidPrice.ToString();
+            paymentRequest.Currency = Currency.TRY.ToString();
+            paymentRequest.BasketId = StringHelpers.GenerateUniqueCode();
+            paymentRequest.PaymentChannel = PaymentChannel.WEB.ToString();
+            paymentRequest.PaymentGroup = PaymentGroup.SUBSCRIPTION.ToString();
+
+            var buyer = new Buyer()
+            {
+                Id = "BY789",
+                Name = "John",
+                Surname = "Doe",
+                GsmNumber = "+905350000000",
+                Email = "email@email.com",
+                IdentityNumber = "74300864791",
+                LastLoginDate = "2015-10-05 12:43:35",
+                RegistrationDate = "2013-04-21 15:12:09",
+                RegistrationAddress = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1",
+                Ip = "85.34.78.112",
+                City = "Istanbul",
+                Country = "Turkey",
+                ZipCode = "3473"
+
+            };
+
+            paymentRequest.Buyer = buyer;
+
+            return paymentRequest;
+        }
         public InstallmentModel CheckInstallments(string binNumber, decimal price)
         {
              
@@ -65,8 +105,11 @@ namespace it_service_app.Services
             Console.WriteLine();
             return resultModel;
         }
-        public PaymentResponseModel Pay(PaymentModel model)
+        public PaymentResponseModel Pay(PaymentModel paymentModel)
         {
+            var request = this.InitialPaymentRequest(paymentModel); // return CreatePaymentRequest
+            var payment = Payment.Create(request, _options);
+
             return null;
         }
     }
