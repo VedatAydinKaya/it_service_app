f2c65dbbb2b07775a662e09fbd8d4394caf09207
Vedat Kaya <vedataydinkayaa@gmail.com>
2022-02-12 20:07:28 +0300

    grid-form editing && update users

--- it_service_app/Areas/Admin/Controllers/UserApiController.cs
+++ it_service_app/Areas/Admin/Controllers/UserApiController.cs
@@ -3,15 +3,17 @@ using it_service_app.Extensions;
 using it_service_app.Models.Identity;
 using it_service_app.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace it_service_app.Areas.Admin.Controllers
 {
     [Route("api/[controller]/[action]")]
-    [ApiController]
     [Authorize(Roles = "Admin")]
     public class UserApiController : ControllerBase // A  base class for an MVC without view support
     {
@@ -29,6 +31,35 @@ namespace it_service_app.Areas.Admin.Controllers
 
             return Ok(DataSourceLoader.Load(data, loadOptions));
         }
+        [HttpPut]
+        public async Task<IActionResult> UpdateUsers(string key, string values) 
+        {
+            var data = _userManager.Users.FirstOrDefault(x => x.Id == key);
+            if (data == null)
+                return StatusCode(StatusCodes.Status409Conflict, new JSonResponseViewModel()
+                {
+                     IsSuccess=false,
+                      ErrorMessage="Kullanıcı bulunamadı"
+                });
+            JsonConvert.PopulateObject(values, data);
+
+            if (!TryValidateModel(data))
+                return BadRequest(ModelState.ToFullErrorString());
+
+            var result=await _userManager.UpdateAsync(data);
+
+            if (!result.Succeeded)
+
+                return BadRequest(new JSonResponseViewModel()
+                {
+                      IsSuccess = false,
+                       ErrorMessage="Kullanıcı guncellenemedi"
+                });
+
+            return Ok(new JSonResponseViewModel());
+
+        }
+
         [HttpGet]
         public IActionResult GetTest()
         {
